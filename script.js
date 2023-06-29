9d4d2db3a4a2c06033c9cfab37d526dab2e8e027
Manav Reja <manav13reja@gmail.com>
2023-06-29 13:12:49 +0530

    fix: minor typos in code

--- script.js
+++ script.js
@@ -1,31 +1,30 @@
-// Todo: Make M+ M- and MC functional
-let string = "";
+let inputString = "";
 let buttons = document.querySelectorAll('.button');
 Array.from(buttons).forEach((button)=>{
   button.addEventListener('click', (e)=>{
     if(e.target.innerHTML == '='){
-      string = eval(string);
-      document.querySelector('input').value = string;
+      inputString = eval(inputString);
+      document.querySelector('input').value = inputString;
     }
     else if(e.target.innerHTML == 'C'){
-      string = ""
-      document.querySelector('input').value = string;
+      inputString = ""
+      document.querySelector('input').value = inputString;
     }else if(e.target.innerHTML.includes("%")){
-      string = eval(string);
+      inputString = eval(inputString);
      
-      document.querySelector('input').value = string/100  
+      document.querySelector('input').value = inputString/100  
     }
     else{ 
       let domValue = e.target.innerHTML
     if( document.querySelector('input').value ){
-      string = document.querySelector('input').value 
+      inputString = document.querySelector('input').value 
     }if(e.target.innerHTML.includes("M+")){
       domValue = "+"
     }else if(e.target.innerHTML.includes("M-")){
       domValue = "-"
     }
-    string = string + domValue;
-    document.querySelector('input').value = string;
+    inputString = inputString + domValue;
+    document.querySelector('input').value = inputString;
       }
   })
 })
